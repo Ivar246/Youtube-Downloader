e54cbf2455e76afa7c29ca3d520dde672b157981
Ravi Shrestha <ravistha869@gmail.com>
2022-04-23 09:56:16 +0545

    a simple youtube video downloader

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,20 +1,18 @@
 import os 
-
-
-
 from pytube import YouTube 
+
+
 SAVE_PATH = os.getcwd()
-print(SAVE_PATH)
 link=input("Enter the video link> ")
 
 
 yt = YouTube(link)
-print("title: ", yt.title)
-print("length: ", yt.length)
-print("views: ", yt.views)
+print("title: ", yt.title)  # print title of video
+print("length: ", yt.length) # print length of vido in second
+print("views: {:,}".format(yt.views))   # print total number ov views of video
 
 # print(yt.streams)
 a=yt.streams.filter(progressive=True, file_extension='mp4')
 b=a.order_by('resolution').desc().first()
-print(b)
-# b.download(SAVE_PATH, 'videoFilename', 'mp4')
+
+b.download(SAVE_PATH, 'videoFilename', 'mp4')
