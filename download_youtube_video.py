d9337a61ee7f80272d66334b4aa0a9986269ab98
IVAR AHTSERHS <ravistha869@gmail.com>
2023-01-25 10:08:02 +0545

    update

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,26 +1,23 @@
 import os 
-from pytube import YouTube, Search
+from pytube import YouTube
 
 
 SAVE_PATH = os.getcwd()
-link=input("Enter the video link> ")
-
-yt = YouTube(link)
-
-# print("title: ", yt.title)  # print title of video
-# print("length: ", yt.length) # print length of vido in second
-# print("views: {:,}".format(yt.views))   # print total number ov views of video
-
-# print('all streams\n', [stream for stream in  yt.streams.order_by("resolution")])
-a=yt.streams.filter(progressive=True, file_extension='mp4')
-# print('\nfiltered stream\n', a)
-# print('\nordered resolution:\n', a.order_by('resolution'))
-
-# print('\nordered disc:\n', a.order_by('resolution').desc())
-b=a.order_by('resolution').desc().first()
-# print("\nb_first:\n", b)
-b.download(SAVE_PATH, 'videoFilename', 'mp4')
-print('secess')     
 
 
-# urllib.error.URLErrorown
+# def downloadVideo(link):
+    
+#     yt = YouTube(link)
+#     downloadObj = yt.streams.filter(progressive=True)
+   
+#     try:
+#         downloadObj.download(SAVE_PATH)
+#     except: 
+#         print('Error has occured.')
+    
+#     print("Download Successfully")
+
+
+url=input("Enter the video link > ")
+yt = YouTube(url)
+downloadObj = yt.streams.filter(progressive=True)
