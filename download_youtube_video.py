8a3879a439f26427b3251fd0ea2531a819172189
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-17 21:58:38 +0545

    youtube video downloader cli

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -5,19 +5,29 @@ from pytube import YouTube
 SAVE_PATH = os.getcwd()
 
 
-# def downloadVideo(link):
-    
-#     yt = YouTube(link)
-#     downloadObj = yt.streams.filter(progressive=True)
+def downloadVideo(link="https://youtu.be/tPEE9ZwTmy0"):
+    yt = YouTube('https://youtu.be/tPEE9ZwTmy0')
+    downloadObj = yt.streams.filter(progressive=True)
+    destination = ''
    
-#     try:
-#         downloadObj.download(SAVE_PATH)
-#     except: 
-#         print('Error has occured.')
+    try:
+        
+       downloadObj = yt.streams.filter(progressive=True)
+       res=[r.resolution for r in downloadObj]
+       
+       print("Select the video resolution: ")
+       for item in res.enumerate(res, 1):
+           print(f'{item[0]}. {item[1]}')
+       
+       userChoice = int(input('Enter your choice: '))
+       chosenStream = [stream for stream in downloadObj if stream.resolutiion == res[userChoice-1]][0]
+       destination = chosenStream.download(SAVE_PATH)  
+                     
+    except: 
+        print('Error has occured.')
     
-#     print("Download Successfully")
+    print("Download Successfully")
+    print(f"File has been saved in {destination}")
 
-
-url=input("Enter the video link > ")
-yt = YouTube(url)
-downloadObj = yt.streams.filter(progressive=True)
+if '__name__' == '__main__':
+    downloadVideo()
